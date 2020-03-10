8f1254f6ba7e50d36b024c487db994025de89c14
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2020-03-10 17:50:05 +0100

    Initial commit

---
+++ src/main/kotlin/pl/put/poznan/pz/doggo/DoggoApplication.kt
@@ -0,0 +1,11 @@
+package pl.put.poznan.pz.doggo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DoggoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DoggoApplication>(*args)
+}
---
+++ src/test/kotlin/pl/put/poznan/pz/doggo/DoggoApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.put.poznan.pz.doggo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DoggoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
