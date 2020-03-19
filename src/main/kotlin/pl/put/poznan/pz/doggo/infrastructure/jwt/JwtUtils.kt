a81781fae834c78a97d2984bf98b8c3ae206289b
Adrian Glapiński <adriangl1997@gmail.com>
2020-03-19 20:18:16 +0100

    Optimize imports

--- src/main/kotlin/pl/put/poznan/pz/doggo/infrastructure/jwt/JwtUtils.kt
+++ src/main/kotlin/pl/put/poznan/pz/doggo/infrastructure/jwt/JwtUtils.kt
@@ -4,7 +4,6 @@ import io.jsonwebtoken.*
 import io.jsonwebtoken.security.Keys
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
 import pl.put.poznan.pz.doggo.modules.auth.security.userdetails.CustomUserDetails
