57fce270b41a06ce3aa34da3f02bb5f07472b9b2
Adrian Glapiński <adriangl1997@gmail.com>
2020-03-21 20:42:11 +0100

    Remove ExceptionHandlingAdvice

--- src/main/kotlin/pl/put/poznan/pz/doggo/infrastructure/exceptions/ExceptionHandlingAdvice.kt
+++
@@ -1,8 +0,0 @@
-package pl.put.poznan.pz.doggo.infrastructure.exceptions
-
-import org.springframework.web.bind.annotation.ControllerAdvice
-
-@ControllerAdvice
-class ExceptionHandlingAdvice {
-
-}
